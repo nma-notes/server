2bdbf8fc659246a262ff558d53a89143f0722fd7
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-09 12:06:35 +0300

    add find by email method to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findFirst({ where: { email } });
+  }
+}
