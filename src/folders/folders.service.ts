0a3f5f34f61fa7585e15b5d4b1f01620e1d85dc7
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-10 19:26:22 +0300

    add find all method

--- src/folders/folders.service.ts
+++ src/folders/folders.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import CreateFolderDto from './dto/create.folder.dto';
+import { User } from '.prisma/client';
 
 @Injectable()
 export class FoldersService {
   constructor(private prisma: PrismaService) {}
+
+  async findAll(owner: User, take: number, skip: number) {
+    return this.prisma.folder.findMany({
+      take,
+      skip,
+      where: { ownerId: owner.id },
+    });
+  }
 
   async create(folder: CreateFolderDto) {
     return this.prisma.folder.create({ data: { ...folder } });
