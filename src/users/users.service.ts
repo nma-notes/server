64e6b1d06925494b2a801a0b84c84ce7213738db
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-10 18:48:06 +0300

    add create user method to service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import CreateUserDto from './dto/create.user.dto';
+import { hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -7,5 +9,15 @@ export class UsersService {
 
   async findByEmail(email: string) {
     return this.prisma.user.findFirst({ where: { email } });
+  }
+
+  async create(user: CreateUserDto) {
+    const { email, password } = user;
+
+    if (await this.findByEmail(email))
+      throw new BadRequestException('Email already exists');
+
+    const passwordHash = hashSync(password, 10);
+    return this.prisma.user.create({ data: { email, passwordHash } });
   }
 }
