b4bff91982f9cba4519ab3a61f56c4fba316d230
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-12 09:39:30 +0300

    add find all action

--- src/folders/folders.controller.ts
+++ src/folders/folders.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Post,
+  UseGuards,
+  Body,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { FoldersService } from './folders.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import CreateFolderDto from './dto/create.folder.dto';
@@ -7,6 +15,18 @@ import CreateFolderDto from './dto/create.folder.dto';
 @UseGuards(JwtAuthGuard)
 export class FoldersController {
   constructor(private foldersService: FoldersService) {}
+
+  DEFAULT_TAKE_VALUE = 25;
+  DEFAULT_SKIP_VALUE = 0;
+
+  @Get('/')
+  async findAll(@Query() params, @Request() req) {
+    return this.foldersService.findAll(
+      req.user,
+      +params['take'] || this.DEFAULT_TAKE_VALUE,
+      +params['skip'] || this.DEFAULT_SKIP_VALUE,
+    );
+  }
 
   @Post('/')
   async create(@Body() createFolderDto: CreateFolderDto, @Request() req) {
